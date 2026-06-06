adeb18507924ada36d9f5c9d8646606736de7863
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2026-06-07 08:11:53 +0900

    update

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: ['e2e/**'],
+    exclude: [...configDefaults.exclude, 'e2e/**'],
   },
 });
